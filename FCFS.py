03ff290417dca1936c13dfe75352e76f5faf5404
kanwalabbas <38763588+kanwalabbas@users.noreply.github.com>
2018-05-04 22:13:21 +0500

    A bit easy

--- FCFS.py
+++ FCFS.py
@@ -12,7 +12,7 @@ turn_arround=[0]*inp
 while i<inp:
     arrival_time[i]=input("Enter the arrival time:")
     burst_time[i]=input("Enter the burst time:")
-    i=i+1
+    i=i+1 
     
 for i in range(inp):
     for j in range(inp-i):
