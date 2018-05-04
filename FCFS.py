20b71d38609134c4d218171be225073e2144d96d
kanwalabbas <38763588+kanwalabbas@users.noreply.github.com>
2018-05-04 10:12:01 +0500

    Update FCFS.py

--- FCFS.py
+++ FCFS.py
@@ -10,7 +10,6 @@ waiting_time=[0]*inp
 turn_arround=[0]*inp
 
 while i<inp:
-    arrival_time=input("Enter the arrival time:")
     burst_time=input("Enter the burst time:")
     i=i+1
     
@@ -20,12 +19,18 @@ for i in range(inp):
                 temp=arrival_time[j]
                 arrival_time[j]=arrival_time[j+1]
                 arrival_time[j+1]=temp
+                if arrival_time[j] ==arrival_time[j+1]:
+                     temp = burst_time[j+1]
+		             burst_time[j+1] = burst_time[j]
+		             burst_time[j] = temp
+				
                 print("Arrival_time", arrival_time[j], arrival_time[j+1])
+                print("Burst_time", Burst_time[j],burst_time[j+1])
                 
 waiting_time[0]=0
 for i in range(inp):
     waiting_time[i]=0
-    for j in range(inp-i):
+    for j in range(i):
          waiting_time[i]+=burst_time[j]
          print("Waiting_time",waiting_time[i])
          
@@ -33,5 +38,4 @@ turn_around[0]=0
 for i in range(inp):
          turn_around[i]+=waiting_time[i]+burst_time[i]
          print("Turn_around",turn_around[i])
-    
-                           
+   
